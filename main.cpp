dd7782c829688010c7d8cee5a7dde3f12f7578b0
MorisZheng <s04044mort@gmail.com>
2023-03-25 14:24:29 +0800

    up

--- main.cpp
+++ main.cpp
@@ -5,26 +5,26 @@ AnalogOut aout(PA_4);
 
 int main()
 {
-   float sample[400];
+   float sample[100];
 
-   for (int i = 0; i < 400; i++){
-        if (i < 100)
-            sample[i] = (float)i / 100;
-        else if (i < 200)
+   for (int i = 0; i < 100; i++){
+        if (i < 25)
+            sample[i] = (float)i / 25;
+        else if (i < 50)
             sample[i] = 1.;
-        else if (i < 300)
-            sample[i] = 1. - (i - 200.) / 100;
+        else if (i < 75)
+            sample[i] = 1. - (i - 50.) / 25;
         else
             sample[i] = 0;
    }
-   PWM1.period_ms(4);
+   PWM1.period_ms(5);
    int cnt = 0;
    while (1)
    {
-      PWM1.pulsewidth_us(4000 * sample[cnt]);
+      PWM1.pulsewidth_us(5000 * sample[cnt]);
       aout = PWM1;
       cnt++;
-      if (cnt == 400) cnt = 0;
+      if (cnt == 100) cnt = 0;
       ThisThread::sleep_for(10ms);
    }
 }
