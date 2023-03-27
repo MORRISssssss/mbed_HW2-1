acad90365b3371b4ad9c2110e5dfbad54bbbf5b3
MorisZheng <s04044mort@gmail.com>
2023-03-27 16:31:57 +0800

    up

--- main.cpp
+++ main.cpp
@@ -1,30 +1,40 @@
 #include "mbed.h"
 
-PwmOut PWM1(D6);
+PwmOut LED(D6);
 AnalogOut aout(PA_4);
+
+Thread LEDThread; 
+
+float sample[20];
+
+void pwmLED ()
+{
+    int cnt = 0;
+    while (true)
+    {
+        LED.pulsewidth_us(5000 * sample[cnt]);
+        cnt++;
+        if (cnt == 20) cnt = 0;
+        ThisThread::sleep_for(50ms);
+    }
+}
 
 int main()
 {
-   float sample[100];
-
-   for (int i = 0; i < 100; i++){
-        if (i < 25)
-            sample[i] = (float)i / 25;
-        else if (i < 50)
+   for (int i = 0; i < 20; i++){
+        if (i < 5)
+            sample[i] = (float)i / 5;
+        else if (i < 10)
             sample[i] = 1.;
-        else if (i < 75)
-            sample[i] = 1. - (i - 50.) / 25;
+        else if (i < 15)
+            sample[i] = 1. - (i - 10.) / 5;
         else
             sample[i] = 0;
    }
-   PWM1.period_ms(5);
-   int cnt = 0;
+   LED.period_ms(5);
+   LEDThread.start(pwmLED);
    while (1)
    {
-      PWM1.pulsewidth_us(5000 * sample[cnt]);
-      aout = PWM1;
-      cnt++;
-      if (cnt == 100) cnt = 0;
-      ThisThread::sleep_for(10ms);
+      aout = LED;
    }
 }
